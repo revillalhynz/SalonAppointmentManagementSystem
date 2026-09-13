830f38f301f76c2940676c8030113a1c1b07c306
Revilla, Lhynz Bhern <lhynzbhernrevilla16@gmail.com>
2026-09-13 17:19:18 +0800

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -92,7 +92,6 @@ namespace SalonAppointmentManagementSystem
                             default:
                                 Console.WriteLine("Invalid Input....");
                                 break;
-
                         }
                     }
                     else
@@ -112,59 +111,136 @@ namespace SalonAppointmentManagementSystem
                         Console.WriteLine("Name cannot be Empty.");
                     }
                     else
-                        Customer = inputName; 
-                        Console.WriteLine($"Welcome {Customer}! Please Choose:");
+                        Customer = inputName;
+                    Console.WriteLine($"Welcome {Customer}! Please Choose:");
 
-                        Console.WriteLine("Type 1 - to book an appoinment");
-                        Console.WriteLine("Type 2 - to view appoinment details");
-                        Console.WriteLine("Type 3 - to book exit");
+                    Console.WriteLine("Type 1 - to book an appoinment");
+                    Console.WriteLine("Type 2 - to view appoinment details");
+                    Console.WriteLine("Type 3 - to book exit");
 
-                        Console.Write("Enter your Choice: ");
-                        int customerChoice = Convert.ToInt16(Console.ReadLine());
+                    Console.Write("Enter your Choice: ");
+                    int customerChoice = Convert.ToInt16(Console.ReadLine());
 
-                        switch (customerChoice)
-                        {
-                            case 1:
-                                if (bookedService != "None")
+                    switch (customerChoice)
+                    {
+                        case 1:
+                            if (bookedService != "None")
                             {
                                 Console.WriteLine("There's an Existing Appointment. The Admin must clear-out the details before you booked again.");
-                                    //Ma'am since basic pa lang po tayo, nilagay ko po muna ito, since wala po ring example na array sa ginawa po natin.
-                                break;                  
+                                //Ma'am since basic pa lang po tayo, nilagay ko po muna ito, since wala po ring example na array sa ginawa po natin.
+                                break;
+                            }
+                            Console.WriteLine("Select Service:");
+                            Console.WriteLine("1- Haircut & Styling (PHP 250)");
+                            Console.WriteLine("2- Hair Color (PHP 1,5000)");
+                            Console.WriteLine("3- Manicure & Pedicure (PHP 450)");
+                            Console.Write("Enter your Choice: ");
+                            int serviceChoice = Convert.ToInt16(Console.ReadLine());
+
+
+                            switch (serviceChoice)
+                            {
+                                case 1:
+                                    bookedService = "Haircut & Styling";
+                                    bookedPrice = "Php 250";
+                                    break;
+                                case 2:
+                                    bookedService = "Hair Color";
+                                    bookedPrice = "PHP 1,5000";
+                                    break;
+                                case 3:
+                                    bookedService = "Manicure & Pedicure";
+                                    bookedPrice = "PHP 450";
+                                    break;
+                                default:
+                                    bookedService = "Haircut & Styling";
+                                    bookedPrice = "Php 250";
+                                    break;
                             }
 
+                            Console.Write("Enter Appointment Day(Monday to Saturday)");
+                            string inputDay = Console.ReadLine();
 
-                        
-                            
+                            if (inputDay == "Monday" || inputDay == "Tuesday" || inputDay == "Wednesday" || inputDay == "Thursday" || inputDay == "Friday" || inputDay == "Saturday")
+                            {
+                                appointmentDay = inputDay;
+                            }
+                            else
+                            {
+                                Console.WriteLine("Invalid DAY. Our Salon is closed every Sunday.");
+                                bookedService = "None";
+                                bookedPrice = "None";
+                                break;
+                            }
 
+                            Console.WriteLine("Select Time Slots");
+                            Console.WriteLine("1 - 9:00 AM");
+                            Console.WriteLine("2 - 1:00 PM");
+                            Console.WriteLine("3 - 2:OO PM");
+                            Console.WriteLine("4 - 4:OO PM");
+                            int itemChoice = Convert.ToInt16(Console.ReadLine());
 
+                            switch (itemChoice)
+                            {
+                                case 1:
+                                    appointmentTime = "9:00 AM";
+                                    break;
+                                case 2:
+                                    appointmentTime = "1:00 PM";
+                                    break;
+                                case 3:
+                                    appointmentTime = "2:00 PM";
+                                    break;
+                                case 4:
+                                    appointmentTime = "4:00 PM";
+                                    break;
+                                default:
+                                    appointmentTime = "9:00 AM";
+                                    break;
+                            }
 
+                            Console.WriteLine("Appointment sucessfully booked!");
+                            break;
 
+                        case 2:
+                            if (bookedService == "None")
+                            {
+                                Console.WriteLine("You don't have any apointment yet.");
+                            }
+                            else
+                            {
+                                Console.WriteLine($"Service: {bookedService}");
+                                Console.WriteLine($"Price: {bookedPrice}");
+                                Console.WriteLine($"Day: {appointmentDay}");
+                                Console.WriteLine($"Time: {appointmentTime}");
+                            }
+                            break;
 
+                        case 3:
+                            Console.WriteLine("Thank you for using Salon Appointment Management System");
+                            break;
 
+                        default:
+                            Console.WriteLine("Incorrect input...");
+                            break;
                     }
-                            
-
-
-
-
-
                 }
-
-
-
-
-
-
-
-                }
-
-
-
-
-
-
-
+                else
+            {
+                Console.WriteLine("Invalid input. Please enter 1, 2, or x.");
             }
+
+            Console.WriteLine();
+            Console.WriteLine("Enter 1 - for Admin Login");
+            Console.WriteLine("Enter 2 - for Customer Login");
+            Console.WriteLine("Enter x - to exit");
+            Console.Write("User Input: ");
+            checkLogin = Console.ReadLine();
+        }
+
+            Console.WriteLine("Program ended. Goodbye!");
         }
     }
-    
+}
+
+ 
