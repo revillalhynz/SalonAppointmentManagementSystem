fc9d453e29ec2318821add1e7c4490777f2d0f81
Revilla, Lhynz Bhern <lhynzbhernrevilla16@gmail.com>
2026-09-13 14:40:20 +0800

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -1,11 +1,14 @@
-﻿namespace SalonAppointmentManagementSystem
+using System;
+using System.ComponentModel.Design;
+
+namespace SalonAppointmentManagementSystem
 {
     internal class Program
     {
 
         static void Main(string[] args)
         {
-            Console.WriteLine("Welcome to Salon Appoinment Management System");
+            Console.WriteLine("Welcome to Salon Appointment Management System");
 
             Console.WriteLine("Enter 1 - for Admin Login");
             Console.WriteLine("Enter 2 - for Customer Login");
@@ -16,52 +19,134 @@
             string adminUsername = "Lhynz";
             string adminPassword = "1234";
 
-            string Customer = "ChaCha";
-            string bookService = "None";
+            string Customer = "None";
+            string bookedService = "None";
+            string bookedPrice = "PHP 0";
             string appointmentTime = "None";
+            string appointmentDay = "None";
 
             string checkLogin = Console.ReadLine();
 
 
             while (checkLogin != "x")
             {
-                if (checkLogin == "1") ;
+                if (checkLogin == "1")
                 {
                     Console.Write("Enter Admin Username: ");
                     string userInput = Console.ReadLine();
 
                     Console.Write("Enter Admin Password: ");
-                    string inputPass= Console.ReadLine();
+                    string inputPass = Console.ReadLine();
 
-                    if (adminUsername == userInput && adminPassword == inputPass)
+                    if (userInput == adminUsername && inputPass == adminPassword)
                     {
-                        Console.Write("Welcome Admin!");
+                        Console.WriteLine("Welcome Admin!");
                         Console.WriteLine("Enter 1 - to view current Appointment");
                         Console.WriteLine("Enter 2 - to view clear Appointment");
-                        
+
                         Console.Write("Enter your choice: ");
                         int adminChoice = Convert.ToInt16(Console.ReadLine());
-                    }
 
-                    switch (adminChoice)
+                        switch (adminChoice)
+                        {
+                            case 1:
+                                if (bookedService == "None")
+                                {
+                                    Console.WriteLine("No Appointment has been booked yet!");
+                                }
+                                else
+                                {
+                                    Console.WriteLine("Current Appointment: ");
+                                    Console.WriteLine($"Customer Name: {Customer}");
+                                    Console.WriteLine($"Service Booked: {bookedService}");
+                                    Console.WriteLine($"Appointment Time: {appointmentTime}");
+                                }
+                                break;
+                            case 2:
+                                if (bookedService == "None")
+                                {
+                                    Console.WriteLine("No Appointment to Clear.");
+                                }
+                                else
+                                {
+                                    Console.Write($"Are you sure you want to clear the appointment for {Customer}? (y/n)");
+
+                                    string clearChoice = Console.ReadLine();
+
+                                    if (clearChoice == "y")
+                                    {
+                                        bookedService = "None";
+                                        bookedPrice = "PHP 0";
+                                        appointmentDay = "None";
+                                        appointmentTime = "None";
+                                        Console.WriteLine("The Appointment has been cleared successfully!");
+
+                                    }
+                                    else
+                                    {
+                                        Console.WriteLine("Clearing Appointment has been cancelled.");
+                                    }
+                                }
+                                break;
+
+                            default:
+                                Console.WriteLine("Invalid Input....");
+                                break;
+
+                        }
+                    }
+                    else
                     {
-                        case 1:
-                            Console.WriteLine("Current Appointment: ");
-                            Console.WriteLine($"Customer Name: {Customer}");
-                            Console.WriteLine($"Service Booked: {bookService}");
-                            Console.WriteLine($"Appointment Time: {appointmentTime}");
-                            break;
-
-                        case 2:
-                            bookService = "None";
-                            appointmentTime = "None";
-                            Console.WriteLine("The Appointment has been cleared successfully!");
-                            break;
-
-                        default:
-                            Console.WriteLine("Invalid Input....");
-                            break;
+                        Console.WriteLine("Invalid Information.");
+                        break;
                     }
+                }
+
+                else if (checkLogin == "2")
+                {
+                    Console.Write("Enter your Name");
+                    string inputName = Console.ReadLine();
+
+                    if (inputName == "" || inputName == null)
+                    {
+                        Console.WriteLine("Name cannot be Empty.");
+                    }
+                    else
+                        Customer = inputName; 
+                        Console.WriteLine($"Welcome {Customer}! Please Choose:");
+
+                        Console.WriteLine("Type 1 - to book an appoinment");
+                        Console.WriteLine("Type 2 - to view appoinment details");
+                        Console.WriteLine("Type 3 - to book exit");
+
+                        Console.Write("Enter your Choice: ");
+                        int customerChoice = Convert.ToInt16(Console.ReadLine());
+
+                        switch (customerChoice)
+                        {
+                            case 1:
+                                if (bookedService != "None")
+                            {
+                                Console.WriteLine("There's an Existing Appointment. The Admin must clear-out the details before you booked again.");
+                                    //Ma'am since basic pa lang po tayo, nilagay ko po muna ito, since wala po ring example na array sa ginawa po natin.
+                                break;                  
+                            }
+
+
+                        
+                            
+
+
+
+
+
+
+                    }
+                            
+
+
+
+
 
                 }
 
@@ -71,15 +156,15 @@
 
 
 
+                }
+
+
+
+
+
+
 
             }
-
-
-
-
-
-
-
         }
     }
-}
+    
